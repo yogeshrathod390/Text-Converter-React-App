6b731b5ce305231c3f0c6968c371cbc633367270
unknown <yogeshrathod390@gmail.com>
2022-12-30 18:12:16 +0530

    my first textutils reactwebsite

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
+import { useState } from "react";
 import "./App.css";
+// import About from "./components/About";
 import NavBar from "./components/NavBar";
 import TextBox from "./components/TextBox";
+import Alert from "./components/Alert"; 
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+// } from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState("light");
+
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null)
+    }, 1500)
+
+  };
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "black";
+      showAlert("Dark mode hass been enable", "success");
+      document.title = "Textutils-Dark mode"
+    } else{
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode hass been enable", "success");
+      document.title = "Textutils-Light mode"
+    }
+  };
+
   return (
-    <div>
-      
+    <>
+      {/* <Router> */}
       <NavBar
-        title="TextConverter"
+        mode={mode}
+        toggleMode={toggleMode}
+        title="Textutils"
         home="Home"
-        link="Link"
+        about="About"
         dropdown="Dropdown"
         action="Action"
         Anotheraction="Another Action"
         Somethingelsehere="Something else here"
         search="Search"
-       
       />
-      <TextBox heading="Text converter box"></TextBox>
-    </div>
+      <Alert alert={alert}></Alert>
+      <div className="container my-3">
+     {/* <Routes>
+        <Route each path="/about" element={<About></About>} />
+        <Route each path="/" element= {<TextBox showAlert={showAlert} mode={mode} heading="Enter the text to analyze below"></TextBox>} />
+      </Routes> */}
+      <TextBox showAlert={showAlert} mode={mode} heading="Enter the text to analyze below"></TextBox>}
+      </div>
+     {/* </Router> */}
+      </>
   );
 }
 
--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -1,32 +1,168 @@
 import React, { useState } from "react";
 
 export default function TextBox(props) {
-  const [text, setText] = useState();
+  const [text, setText] = useState(" ");
 
   const onHandleUpClick = () => {
     const convertup = text.toUpperCase();
     setText(convertup);
+    if (text.length > 1) {
+      props.showAlert("converted to uppercase", "success");
+    } else {
+      return( props.showAlert("Put some text", "danger"));
+    }
   };
+
+  const onHandleLoClick = () => {
+    const convertLo = text.toLowerCase();
+    setText(convertLo);
+    if (text.length > 1) {
+      props.showAlert("converted to Lowercase", "success");
+    } else {
+      return( props.showAlert("Put some text", "danger"));
+    }
+   
+  };
+
+  const onHandleClearClick = () => {
+    const convertup = "";
+    setText(convertup);
+    if (text.length > 1) {
+      props.showAlert("Text deleted", "success");
+    } else {
+      return( props.showAlert("Put some text", "danger"))
+    }
+    
+  };
+  const onHandleCopyClick = () => {
+    const copyText = document.getElementById("myBox");
+    copyText.select();
+    navigator.clipboard.writeText(text.value);
+    if (text.length > 1) {
+      props.showAlert("text copyd", "success");
+    } else {
+      return( props.showAlert("Put some text", "danger"))
+    }
+   
+  };
+  const onHandleCleanSpaceClick = () => {
+    const ClearSpace = text.split(/[ ]+/);
+    setText(ClearSpace.join(" "));
+    if (text.length > 1) {
+      props.showAlert("Space clean", "success");
+    } else {
+      return (
+        props.showAlert("Put some text", "danger")
+      )
+    }
+  };
+
   const onHandleChange = (event) => {
     setText(event.target.value);
   };
+
+  // const[ myTextBoxStyle, setmyTextBoxStyle] =  useState({
+  //  color: "black",
+  //   backgroundColor: "white",
+  // })
+
+  // const OnClickDarkMode = () => {
+  //   if(myTextBoxStyle.color === "black"){
+  //     setmyTextBoxStyle({
+  //       color: "white",
+  //     backgroundColor: "black"})
+  //   }
+  //   else if(myTextBoxStyle.color ==="white"){
+  //     setmyTextBoxStyle({
+  //       color: "black",
+  //     backgroundColor: "white"})
+  //   }
+  // }
   return (
-    <div className="container my-3">
-      <div className="mb-3">
-        <h1>{props.heading}</h1>
-        <label htmlFor="myBox" className="form-label"></label>
-        <textarea
-        placeholder="Enter Text Here"
-          value={text}
-          onChange={onHandleChange}
-          className="form-control"
-          id="myBox"
-          rows="10"
-        ></textarea>
+    <>
+      <div
+        className="container my-3"
+        style={{ color: props.mode === "dark" ? "white" : "black" }}
+      >
+        {" "}
+        {/*style={myTextBoxStyle} */}
+        <div className="mb-3">
+          <h1>{props.heading}</h1>
+          <label htmlFor="myBox" className="form-label"></label>
+          <textarea
+            style={{
+              backgroundColor: props.mode === "dark" ? "black" : "white",
+              color: props.mode === "light" ? "black" : "white",
+            }}
+            value={text}
+            onChange={onHandleChange}
+            className="form-control"
+            id="myBox"
+            rows="10"
+          ></textarea>
+        </div>
+        <button
+          onClick={onHandleUpClick}
+          type="button"
+          className="btn btn-danger my-3 mx-3"
+        >
+          Uppercase Convert
+        </button>
+        <button
+          onClick={onHandleLoClick}
+          type="button"
+          className="btn btn-danger  my-3 mx-3 "
+        >
+          Lowercase Convert
+        </button>
+        <button
+          onClick={onHandleCopyClick}
+          type="button"
+          className="btn btn-danger  my-3 mx-3"
+        >
+          Copy Text
+        </button>
+        <button
+          onClick={onHandleCleanSpaceClick}
+          type="button"
+          className="btn btn-danger  my-3 mx-3"
+        >
+          Clear Spaces
+        </button>
+        <button
+          onClick={onHandleClearClick}
+          type="button"
+          className="btn btn-danger  my-3 mx-3"
+        >
+          Clear All
+        </button>
       </div>
-      <button onClick={onHandleUpClick} className="btn btn-primary">
-        Convert Button
-      </button>
-    </div>
+      <div
+        className="container my-3"
+        style={{ color: props.mode === "dark" ? "white" : "black" }}
+      >
+        {/*style={myTextBoxStyle} */}
+        <h2>Your text summary</h2>
+        <p>
+          {text.split(/\s+/).filter((element) =>{return element.length !== 0}).length} words and {text.length} characteors
+        </p>
+        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <h3>Preview</h3>
+        <p>
+          {text.length > 0
+            ? text
+            : "Enter something in the textbox to preview it here"}
+        </p>
+      </div>
+      <div>
+        {/* <button
+          onClick={OnClickDarkMode}
+          type="button"
+          className="btn btn-danger  my-3 mx-3"
+        >
+          Dark Mode
+        </button> */}
+      </div>
+    </>
   );
 }
